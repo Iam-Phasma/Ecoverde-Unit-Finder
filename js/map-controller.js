22a2fca37cf510ac17abcb57f520a7100860d118
Iam-Phasma <ricsonjaymadonza@gmail.com>
2026-08-30 10:51:33 +0800

    feat: update route marker with SVG icon and enhanced styling

--- js/map-controller.js
+++ js/map-controller.js
@@ -921,8 +921,9 @@ export function createMapController() {
     const marker = L.marker(pathLatLngs[0], {
       icon: L.divIcon({
         className: "route-marker",
-        html: "🚗",
-        iconSize: [22, 22],
+        html: '<div class="route-marker-badge"><svg class="route-marker-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/></svg></div>',
+        iconSize: [26, 26],
+        iconAnchor: [13, 13],
       }),
       interactive: false,
     }).addTo(layers.route);
